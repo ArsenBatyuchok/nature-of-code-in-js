0188c95ec8fee6bfee36af14f1ccaa5d5a323a26
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2021-11-27 17:01:38 +0200

    3d ball using 3d vectors

--- public/sketch.js
+++ public/sketch.js
@@ -1,14 +1,16 @@
 let vLocation, velocity;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  background(0)
 
-  vLocation = createVector(0, 0);
-  velocity = createVector(1, 2);
+  vLocation = createVector(0, 0, 0);
+  velocity = createVector(15, 10, 50);
 }
 
 function draw() {
   vLocation.add(velocity);
+  // translate(0, 0, 0);
 
   if (vLocation.x > width || vLocation.x < 0) {
     velocity.x *= -1;
@@ -16,9 +18,16 @@ function draw() {
   if (vLocation.y > height || vLocation.y < 0) {
     velocity.y *= -1;
   }
-  background(0)
-  fill(255)
-  circle(vLocation.x, vLocation.y, 50);
+
+  if (vLocation.z < -3500 || vLocation.z > 0) {
+    velocity.z *= -1;
+  }
+  push();
+    stroke(map(vLocation.x, 0, width, 0, 255), map(vLocation.y, 0, height, 0, 255), map(vLocation.z, 0, 3500, 0, 255))
+    noFill()
+    translate(vLocation.x - width/2, vLocation.y - height/2, vLocation.z);
+    sphere(50, 3, 3);
+  pop();
 }
 
 function windowResized() {
