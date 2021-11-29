e78732324f0cb1241b486f079d71e1d93d8289f7
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2021-11-29 20:09:38 +0200

    arrows experiment

--- public/sketch.js
+++ public/sketch.js
@@ -1,6 +1,7 @@
+const STEP = 100;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background(0)
 
   vLocation = createVector(0, 0);
   velocity = createVector(15, 10, 50);
@@ -8,19 +9,34 @@ function setup() {
 
 function draw() {
   background(255)
-  const mouse = createVector(mouseX, mouseY);
-  const center = createVector(width/2, height/2);
+  for (let x = 0; x < width; x+=STEP) {
+    for (let y = 0; y < height; y+=STEP) {
+      const mouse = createVector(mouseX, mouseY);
+      const position = createVector(x, y);
+      const center = createVector(x, y);
 
-  mouse.sub(center)
-  mouse.normalize()
-  mouse.mult(50)
-
-  translate(width/2, height/2);
-
-  line(0, 0, mouse.x, mouse.y)
-
+      mouse.sub(center)
+      mouse.normalize()
+      mouse.mult(30)
+      drawArrow(position, mouse, color(map(mouseY, 0, height, 0, 255), map(mouseX, 0, height, 0, 255), 200))
+    }
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+function drawArrow(base, vec, color) {
+  push();
+    stroke(color);
+    strokeWeight(3);
+    fill(color);
+    translate(base.x, base.y);
+    line(0, 0, vec.x, vec.y);
+    rotate(vec.heading());
+    let arrowSize = 4;
+    translate(vec.mag() - arrowSize, 0);
+    triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  pop();
 }
