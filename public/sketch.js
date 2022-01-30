4f2c92eb28b8575c01b114bc8ff77b79b7c5b9c9
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-01-30 15:39:50 +0200

    fish

--- public/sketch.js
+++ public/sketch.js
@@ -1,28 +1,34 @@
-let m;
+const fishes = [];
+let time = 0.01;
+const waveTime = 1000;
 
-class Mover {
-  constructor() {
-    this.location = createVector(0, 0);
-    this.velocity = createVector(1, 1);
-    this.mouse = createVector(mouseX, mouseY);
+// should pass start position and velocity
+// should preserve direction
+
+class Fish {
+  constructor(location, velocity, size, color) {
+    this.location = location;
+    this.velocity = velocity;
     this.acceleration = createVector(0, 0);
-    this.direction = createVector(0, 0);
-  }
-
-  setDirectionProperties() {
-    this.mouse.set(mouseX, mouseY);
-    this.direction = p5.Vector.sub(this.mouse, this.location);
-    this.direction.normalize();
+    this.lastRender = Date.now();
+    this.rads = velocity.heading();
+    this.size = size;
+    this.color = color;
   }
 
   update() {
-    this.velocity.limit(10);
-    this.setDirectionProperties();
-    const distance = p5.Vector.sub(this.mouse, this.location).mag();
-    this.direction.mult(map(distance, Math.max(width, height), 0, 0, 1));
-    this.acceleration = this.direction;
-    this.velocity.add(this.acceleration);
-    this.location.add(this.velocity);
+    const diff = Date.now() - this.lastRender;
+    this.lastRender += diff;
+    this.rads += diff / waveTime * (Math.PI * 2);
+    const vxs = this.velocity.x >= 0 ? 1 : -1;
+    const vys = this.velocity.y >= 0 ? 1 : -1;
+    this.acceleration.x = vxs * Math.abs(Math.cos(this.rads));
+    this.acceleration.y = vys * Math.abs(Math.sin(this.rads));
+
+    const v = this.velocity.copy();
+    v.limit(20);
+    v.add(this.acceleration);
+    this.location.add(v);
   }
 
   checkEdges() {
@@ -39,23 +45,38 @@ class Mover {
   }
 
   display() {
-    fill(255);
-    circle(this.location.x, this.location.y, 20);
+    fill(this.color);
+    circle(this.location.x, this.location.y, this.size);
   }
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
+  noStroke()
 
-  m = new Mover();
+  for (let i = 0; i < 20; i += 1) {
+    fishes.push(new Fish(
+      // location
+      createVector(random(0, width), random(0, height)),
+      // velocity
+      createVector(random(5, -5), random(5, -5)),
+      // fish size
+      random(4, 16),
+      // color
+      color(255, 204, 0)
+    ));
+  }
 }
 
 function draw() {
-  background(0);
-  m.update();
-  m.checkEdges();
-  m.display();
+  time += 0.01;
+  background('rgba(0, 0, 0, 0.15)');
+  fishes.forEach(f => {
+    f.update();
+    f.checkEdges();
+    f.display();
+  });
 }
 
 function windowResized() {
