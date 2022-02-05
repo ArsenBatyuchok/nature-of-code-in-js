451e109bcc6ea85f19b5669de02a51f7da2ed7a0
Arsen Batyuchok <arsen.batyuchok@gmail.com>
2022-02-05 09:56:33 +0200

    orbits

--- public/sketch.js
+++ public/sketch.js
@@ -1,88 +1,54 @@
-const fishes = [];
-let time = 0.01;
-const waveTime = 1000;
+let p, s;
 
-// should pass start position and velocity
-// should preserve direction
-
-class Fish {
-  constructor(location, velocity, size, color) {
-    this.location = location;
-    this.velocity = velocity;
-    this.acceleration = createVector(0, 0);
-    this.lastRender = Date.now();
-    this.rads = velocity.heading();
+class Planet {
+  constructor(size) {
     this.size = size;
-    this.color = color;
   }
 
   update() {
-    const diff = Date.now() - this.lastRender;
-    this.lastRender += diff;
-    this.rads += diff / waveTime * (Math.PI * 2);
-    const vxs = this.velocity.x >= 0 ? 1 : -1;
-    const vys = this.velocity.y >= 0 ? 1 : -1;
-    this.acceleration.x = vxs * Math.abs(Math.cos(this.rads));
-    this.acceleration.y = vys * Math.abs(Math.sin(this.rads));
+    push();
+      specularColor(255, 0, 0);
+      pointLight(255, 0, 0, 0, -this.size * 3, this.size * 3);
+      specularColor(0, 255, 0);
+      pointLight(0, 255, 0, 0, this.size * 3, this.size * 3);
+      ambientMaterial(255);
+      rotateY(frameCount * 0.01);
+      sphere(this.size);
+    pop();
+  }
+}
 
-    const v = this.velocity.copy();
-    v.limit(20);
-    v.add(this.acceleration);
-    this.location.add(v);
+class Sputnik {
+  constructor(size, orbit) {
+    this.size = size;
+    this.orbit = orbit;
   }
 
-  checkEdges() {
-    this.checkEdge('x', width);
-    this.checkEdge('y', height);
-  }
-
-  checkEdge(axis, maxValue) {
-    if (this.location[axis] > maxValue) {
-      this.location[axis] = 0;
-    } else if (this.location[axis] < 0) {
-      this.location[axis] = maxValue;
-    }
-  }
-
-  display() {
-    fill(this.color);
-    circle(this.location.x, this.location.y, this.size);
+  update() {
+    push();
+      specularMaterial(255);
+      translate(sin(frameCount * 0.01) * 200, cos(frameCount * 0.01) * 200, sin(frameCount * 0.01) * 150);
+      sphere(this.size);
+    pop();
   }
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   background(0);
-  noStroke()
-
-  for (let i = 0; i < 20; i += 1) {
-    fishes.push(new Fish(
-      // location
-      createVector(random(0, width), random(0, height)),
-      // velocity
-      createVector(random(5, -5), random(5, -5)),
-      // fish size
-      random(4, 16),
-      // color
-      color(255, 204, 0)
-    ));
-  }
+  noStroke();
+  p = new Planet(150);
+  s = new Sputnik(10, 150);
 }
 
 function draw() {
-  time += 0.01;
-  background('rgba(0, 0, 0, 0.15)');
-  fishes.forEach(f => {
-    f.update();
-    f.checkEdges();
-    f.display();
-  });
+  orbitControl();
+  background(`rgba(0, 0, 0, 0.5)`);
+  ambientLight(50);
+  p.update();
+  s.update();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function keyPressed() {
-
 }
